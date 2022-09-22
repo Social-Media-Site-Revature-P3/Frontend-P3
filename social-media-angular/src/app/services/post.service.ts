a83d9d39511aaac2cf4b56ab599a3851c8cc960a
JaredMAdams <jaredmarshalladams@gmail.com>
2022-09-22 13:16:26 -0400

    added followed id interface for getting posts by followed (front page feed), as well as fixing post service and security service

--- social-media-angular/src/app/services/post.service.ts
+++ social-media-angular/src/app/services/post.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { FollowedId } from '../interfaces/followed-id';
 import Post from '../models/Post';
 
 @Injectable({
@@ -9,15 +11,87 @@ import Post from '../models/Post';
 })
 export class PostService {
 
-  postUrl: string = `${environment.baseUrl}/post`
+  postUrl: string = `${environment.baseUrl}/post/`
 
   constructor(private http: HttpClient) { }
 
-  getAllPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.postUrl}`, {headers: environment.headers, withCredentials: environment.withCredentials} )
+  getbyPostId(postId: number): Observable<Post> {
+    return this.http.get<Post>(`${this.postUrl}` + postId, {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
   }
 
-  upsertPost(post: Post): Observable<Post> {
-    return this.http.put<Post>(`${this.postUrl}`, post, {headers: environment.headers, withCredentials: environment.withCredentials})
+  getByUserId(userId: number): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.postUrl}` + "user/" + userId, {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
+  }
+
+  getByFollowed(followedIds: FollowedId[]): Observable<Post[]> {
+    return this.http.post<Post[]>(`${this.postUrl}` + "follow", JSON.stringify(followedIds), {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
+  }
+
+  getByComments(postId: number): Observable<Post> {
+    return this.http.get<Post>(`${this.postUrl}` + "comments/" + postId, {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
+  }
+
+  getAllPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.postUrl}`, {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
+  }
+
+  postComment(comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>(`${this.postUrl}` + "comment", JSON.stringify(comment), {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
+  }
+
+  updatePost(post: Post, postId: number): Observable<Post> {
+    return this.http.put<Post>(`${this.postUrl}` + "update-post/" + postId, JSON.stringify(post), {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
+  }
+
+  deletePost(postId: number) {
+    return this.http.delete(`${this.postUrl}` + postId, {headers: environment.headers, withCredentials: environment.withCredentials})
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    )
+  }
+
+  errorHandl(error: any) {
+    let errorMessage = 'error, try again!';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    }else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log("Error obj: ", error);
+    if(error.status == 500) {alert("Password does not contain the required characters");}
+    //we can check this error object for a code: if(error.status == 401) {alert(acces denied)}
+    console.log(errorMessage);
+    return throwError(errorMessage);
   }
 }
