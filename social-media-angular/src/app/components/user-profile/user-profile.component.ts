a69c4c9a66b5b2fb09d4407dd341e09374db1abe
hazewin <hnguy154@ucr.edu>
2022-09-29 08:06:08 -0700

    update user-profile

--- social-media-angular/src/app/components/user-profile/user-profile.component.ts
+++ social-media-angular/src/app/components/user-profile/user-profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
-import { EditUserProfileComponent } from '../edit-user-profile/edit-user-profile.component';
 import { User } from 'src/app/models/User';
 
 @Component({
@@ -35,38 +34,6 @@ export class UserProfileComponent implements OnInit {
     firstName: "",
     lastName: "",
     password: ""
-  }
-
-  editUserProfile() {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.height= "80%";
-    dialogConfig.width = "60%";
-    // dialogConfig.data = {
-    //   userId: this.currUser.userId,
-    //   email: this.currUser.email,
-    //   nickname: this.currUser.nickname,
-    //   password: this.currUser.password,
-    //   firstName: this.currUser.firstName,
-    //   lastName: this.currUser.lastName,
-    //   aboutMe: this.currUser.aboutMe,
-    //   profilePicutre: this.currUser.profilePicutre
-    // }
-
-    // dialogConfig.data = {
-    //   id: this.currUser.id,
-    //   email: this.currUser.email,
-    //   firstName: this.currUser.firstName,
-    //   lastName: this.currUser.lastName
-    // }
-
-    // Open EditUserProfile component which displays the dialog box
-    let dialogRef = this.dialog.open(EditUserProfileComponent, dialogConfig);
-
-    dialogRef.afterClosed().subscribe(updatedUser => {
-      this.currUser = updatedUser;
-    })
   }
 
   followUser() {
