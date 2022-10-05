069265261b54aabaae37b008c268d74caee532a0
JaredMAdams <109548288+JaredMAdams@users.noreply.github.com>
2022-10-05 15:27:44 -0400

    Update environment.prod.ts

--- social-media-angular/src/environments/environment.prod.ts
+++ social-media-angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "http://localhost:8080",
+  baseUrl: "http://kp-spa-hosting.s3-website.us-east-2.amazonaws.com",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:4200',
